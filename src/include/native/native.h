a7398814292f3fb05409c7496603c99ac55fe56c
Ben Phelps <ben@phelps.io>
2023-06-28 21:44:49 +0300

    add more native helper macros

--- src/include/native/native.h
+++ src/include/native/native.h
@@ -34,7 +34,10 @@ NativeModuleEntry*    findNativeModule(NativeModuleEntry* modules, const char* n
 NativeModuleCallback* findNativeModuleCallback(NativeModuleCallback* callbacks, const char* name);
 ObjTable*             defineNativeModule(NativeModuleEntry* module);
 
+#define lux_value(pos) (args[pos])
+
 #define lux_push(pos, val) (args[pos] = val)
+#define lux_pushValue(pos, val) (args[pos] = val)
 #define lux_pushObject(pos, val) (args[pos] = OBJ_VAL(val))
 #define lux_pushCString(pos, val) (args[pos] = OBJ_VAL(copyString(val, strlen(val))))
 #define lux_pushString(pos, val) (args[pos] = OBJ_VAL(val))
@@ -49,6 +52,10 @@ ObjTable*             defineNativeModule(NativeModuleEntry* module);
 #define lux_isBool(pos) (IS_BOOL(args[pos]))
 #define lux_isNil(pos) (IS_NIL(args[pos]))
 #define lux_isObject(pos) (IS_OBJ(args[pos]))
+#define lux_isArray(pos) (IS_ARRAY(args[pos]))
+#define lux_isTable(pos) (IS_TABLE(args[pos]))
+#define lux_isFunction(pos) (IS_FUNCTION(args[pos]))
+#define lux_isClosure(pos) (IS_CLOSURE(args[pos]))
 
 #define lux_toString(val) (AS_STRING(args[val]))
 #define lux_toCString(val) (AS_CSTRING(args[val]))
@@ -99,12 +106,51 @@ ObjTable*             defineNativeModule(NativeModuleEntry* module);
         return false;                                                            \
     }
 
+#define lux_checkArray(pos)                                                        \
+    if (!lux_isArray(pos)) {                                                       \
+        lux_pushObject(-1, formatString("Argument %d must be a array.", pos + 1)); \
+        return false;                                                              \
+    }
+
+#define lux_checkTable(pos)                                                        \
+    if (!lux_isTable(pos)) {                                                       \
+        lux_pushObject(-1, formatString("Argument %d must be a table.", pos + 1)); \
+        return false;                                                              \
+    }
+
+#define lux_checkFunction(pos)                                                        \
+    if (!lux_isFunction(pos)) {                                                       \
+        lux_pushObject(-1, formatString("Argument %d must be a function.", pos + 1)); \
+        return false;                                                                 \
+    }
+
+#define lux_checkClosure(pos)                                                        \
+    if (!lux_isClosure(pos)) {                                                       \
+        lux_pushObject(-1, formatString("Argument %d must be a closure.", pos + 1)); \
+        return false;                                                                \
+    }
+
 #define lux_checkArgs(count)                                                                    \
     if (argCount != count) {                                                                    \
         lux_pushObject(-1, formatString("Expected %d arguments but got %d.", count, argCount)); \
         return false;                                                                           \
     }
 
+#define lux_checkMinArgs(count)                                                                 \
+    if (argCount < count) {                                                                     \
+        lux_pushObject(-1, formatString("Expected %d arguments but got %d.", count, argCount)); \
+        return false;                                                                           \
+    }
+
 #define lux_error(msg, ...) lux_pushObject(-1, formatString(msg, ##__VA_ARGS__))
+
+#define lux_callClosure(closure, args)                          \
+    do {                                                        \
+        Chunk chunk                 = closure->function->chunk; \
+        chunk.code[chunk.count - 1] = OP_REENTER;               \
+        call(closure, args);                                    \
+        run(true);                                              \
+        *(vm.stackTop - 1 - args) = *(vm.stackTop - 1);         \
+    } while (false)
 
 #endif
